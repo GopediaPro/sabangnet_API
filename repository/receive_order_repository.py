074640abbb26c9594fbbaf79569f05be00436319
김병민 <gimbyeongmin@byeong.local>
2025-06-27 02:13:56 +0900

    read_all add

--- repository/receive_order_repository.py
+++ repository/receive_order_repository.py
@@ -1,7 +1,7 @@
 from typing import Optional
 from core.db import get_async_session
 from models.receive_order.receive_order import ReceiveOrder
-
+from sqlalchemy import select
 
 class CreateReceiveOrder:
     async def create(self, obj_in: ReceiveOrder) -> ReceiveOrder:
@@ -11,6 +11,19 @@ class CreateReceiveOrder:
             await session.commit()
             await session.refresh(obj_in)
             return obj_in
+        except Exception as e:
+            await session.rollback()
+            raise e
+        finally:
+            await session.close()
+    
+    async def read_all(self) -> list[ReceiveOrder]:
+        session = await get_async_session()
+        try: 
+            query = select(ReceiveOrder)
+            result = await session.execute(query)
+            content = result.scalars().all()
+            return content
         except Exception as e:
             await session.rollback()
             raise e
