978b0473de6c44339264435a9fd75ff0ca6f7814
sunsik17 <17sunsik@gmail.com>
2025-07-01 16:01:49 +0900

    feat: add mall price endpoint and integrate with main router

---
+++ api/v1/endpoints/mall_price.py
@@ -0,0 +1,24 @@
+from fastapi import APIRouter, Depends
+from schemas.mall_price.request.create_mall_price_form import CreateMallPriceForm
+from schemas.mall_price.response.setting_mall_price_respones import SettingMallPriceResponse
+from core.db import get_async_session
+from sqlalchemy.ext.asyncio import AsyncSession
+from services.usecase.product_mall_price_usecase import ProductMallPriceUsecase
+
+
+router = APIRouter(
+    prefix="/mall-price",
+    tags=["mall-price"],
+)
+
+def get_product_mall_price_usecase(session: AsyncSession = Depends(get_async_session)) -> ProductMallPriceUsecase:
+    return ProductMallPriceUsecase(session=session)
+
+@router.post("", response_model=SettingMallPriceResponse)
+async def modify_product_name(
+    request: CreateMallPriceForm = Depends(),
+    product_mall_price_usecase: ProductMallPriceUsecase = Depends(get_product_mall_price_usecase)
+):
+    return SettingMallPriceResponse.from_dto(await product_mall_price_usecase.setting_mall_price(
+        products_nm=request.products_nm,
+    ))
