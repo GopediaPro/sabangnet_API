f11ae1f7ed7368f15a3dd8bab952f23547eac572
sunsik17 <17sunsik@gmail.com>
2025-07-01 16:54:35 +0900

    feat: add method to check existence of mall price by product registration ID

--- repository/mall_price_repository.py
+++ repository/mall_price_repository.py
@@ -1,6 +1,6 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 from models.mall_price.mall_price import MallPrice
-from sqlalchemy import insert
+from sqlalchemy import select
 
 class MallPriceRepository:
     def __init__(self, session: AsyncSession):
@@ -11,3 +11,9 @@ class MallPriceRepository:
         await self.session.flush()
         await self.session.commit()
         return mall_price
+    
+    async def exist_mall_price_by_product_registration_raw_data_id(self, product_registration_raw_data_id: int) -> bool:
+        result = await self.session.execute(
+            select(MallPrice).where(MallPrice.product_registration_raw_data_id == product_registration_raw_data_id)
+        )
+        return result.scalar_one_or_none() is not None
