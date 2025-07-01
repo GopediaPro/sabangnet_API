c1d305fe2bd034af237a86f72151b48799d154a8
sunsik17 <17sunsik@gmail.com>
2025-07-01 16:36:18 +0900

    chore: method naming modify

--- api/v1/endpoints/mall_price.py
+++ api/v1/endpoints/mall_price.py
@@ -15,7 +15,7 @@ def get_product_mall_price_usecase(session: AsyncSession = Depends(get_async_ses
     return ProductMallPriceUsecase(session=session)
 
 @router.post("", response_model=SettingMallPriceResponse)
-async def mall_price_setting햐(
+async def mall_price_setting(
     request: CreateMallPriceForm = Depends(),
     product_mall_price_usecase: ProductMallPriceUsecase = Depends(get_product_mall_price_usecase)
 ):
