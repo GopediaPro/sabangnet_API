2a515ace70574eb23665c9dc34f5bfbf3b7c3ced
sunsik17 <17sunsik@gmail.com>
2025-07-03 17:38:56 +0900

    feat: implement XML generation for mall price registration with shop codes

---
+++ utils/make_xml/sabangnet_xml.py
@@ -0,0 +1,29 @@
+from core.settings import SETTINGS
+import xml.etree.ElementTree as ET
+from datetime import datetime
+import os
+
+class SabangnetXml:
+    
+    _COMPAYNY_ID = SETTINGS.SABANG_COMPANY_ID
+    _AUTH_KEY = SETTINGS.SABANG_AUTH_KEY
+    _SEND_GOODS_CD_RT = SETTINGS.SABANG_SEND_GOODS_CD_RT
+
+    
+    def _create_header(self, root):
+        
+        header = ET.SubElement(root, "HEADER")
+        
+        send_company_id = ET.SubElement(header, "SEND_COMPAYNY_ID")
+        send_company_id.text = self._COMPAYNY_ID
+        
+        send_auth_key = ET.SubElement(header, "SEND_AUTH_KEY")
+        send_auth_key.text = self._AUTH_KEY
+        
+        send_date = ET.SubElement(header, "SEND_DATE")
+        send_date.text = datetime.now().strftime("%Y%m%d")
+        
+        send_goods_cd_rt = ET.SubElement(header, "SEND_GOODS_CD_RT")
+        send_goods_cd_rt.text = self._SEND_GOODS_CD_RT if self._SEND_GOODS_CD_RT else "N"
+        
+        return header
