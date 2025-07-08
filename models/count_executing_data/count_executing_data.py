5474a804c811032d60a8bef83ab1c89d651ffe94
sunsik17 <17sunsik@gmail.com>
2025-07-08 16:12:52 +0900

    refactor: move CountExecuting model package

---
+++ models/count_executing_data/count_executing_data.py
@@ -0,0 +1,26 @@
+from sqlalchemy import (
+    BigInteger, SmallInteger, String, Text, Numeric, CHAR, Integer, DateTime, func
+)
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from models.base_model import Base
+
+
+class CountExecuting(Base):
+    """
+    상품 원본 데이터 테이블(count_executing)의 ORM 매핑 모델
+    file 변환 중 실행중인 프로세스 카운트 저장
+    """
+    __tablename__ = "count_executing"
+
+    # 기본 정보
+    id: Mapped[int] = mapped_column(
+        BigInteger, primary_key=True, autoincrement=True)
+    count_nm: Mapped[str] = mapped_column(String(50))
+    count_rev: Mapped[int] = mapped_column(Integer)
+    created_at: Mapped[DateTime] = mapped_column(DateTime, server_default=func.now(), nullable=False)
+    updated_at: Mapped[DateTime] = mapped_column(DateTime, server_default=func.now(), onupdate=func.now(), nullable=False)
+
+    
+
+    
