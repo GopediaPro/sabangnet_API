490743f6ae22ffd3a9ea3ae696c4ed217d9fcbe9
lyckabc <dh2heart@gmail.com>
2025-07-10 17:23:36 +0900

    feat: add bulk operations for DownFormOrder including create, update, and delete methods

--- services/order/down_form_order_create_service.py
+++ services/order/down_form_order_create_service.py
@@ -10,3 +10,13 @@ class DownFormOrderCreateService:
 
     async def create_down_form_order(self, idx: str) -> DownFormOrderDto:
         return await self.down_form_order_repository.create_down_form_order(DownFormOrderDto(idx=idx))
+
+    async def bulk_create_down_form_orders(self, items: list[DownFormOrderDto]):
+        objs = [DownFormOrderDto(**item.model_dump()) for item in items]
+        return await self.down_form_order_repository.bulk_insert(objs)
+
+    async def bulk_update_down_form_orders(self, items: list[DownFormOrderDto]):
+        return await self.down_form_order_repository.bulk_update(items)
+
+    async def bulk_delete_down_form_orders(self, ids: list[int]):
+        return await self.down_form_order_repository.bulk_delete(ids)
