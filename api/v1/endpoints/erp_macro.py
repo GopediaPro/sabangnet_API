a19eafcd0522a36ae750a6473a8283434ce97899
lyckabc <dh2heart@gmail.com>
2025-07-11 14:45:58 +0900

    refactor: streamline excel_run_macro function by integrating process_macro_with_tempfile and simplifying MinIO upload logic

--- api/v1/endpoints/erp_macro.py
+++ api/v1/endpoints/erp_macro.py
@@ -1,10 +1,9 @@
 from fastapi import APIRouter, Depends, UploadFile, File, Form, HTTPException
 from utils.sabangnet_logger import get_logger
-from minio_handler import upload_file_to_minio, get_minio_file_url, temp_file_to_object_name, delete_temp_file, url_arrange
+from minio_handler import upload_and_get_url, url_arrange
 from sqlalchemy.ext.asyncio import AsyncSession
 from core.db import get_async_session
-from services.order.macros.order_macro_service import run_macro
-from datetime import datetime
+from services.order.macros.order_macro_service import process_macro_with_tempfile
 
 logger = get_logger(__name__)
 
@@ -24,21 +23,9 @@ async def excel_run_macro(
     """
     try:
         # Excel file template_code 에 따라 실행
-        logger.info(f"Excel file template_code 에 따라 실행")
-        logger.info(f"file: {file}")
-        file_name = file.filename
-        logger.info(f"file_name: {file_name}")
-        temp_upload_file_path = temp_file_to_object_name(file)
-        file_path = await run_macro(session, template_code, temp_upload_file_path)
-        delete_temp_file(temp_upload_file_path)
-        logger.info(f"file_name 실행 후 파일 삭제")
+        file_name, file_path = await process_macro_with_tempfile(session, template_code, file)
         # minio 업로드
-        date_now = datetime.now().strftime("%Y%m%d%H%M%S")
-        minio_object_name = f"excel/{template_code}/{date_now}_{file_name}"
-        object_name = upload_file_to_minio(file_path, minio_object_name)
-        delete_temp_file(file_path)
-        # minio 파일 위치 반환
-        file_url = get_minio_file_url(object_name)
+        file_url, minio_object_name = upload_and_get_url(file_path, template_code, file_name)
         return {"file_url": url_arrange(file_url), "object_name": minio_object_name, "template_code": template_code}
     except Exception as e:
         logger.error(f"excel_run_macro error: {e}")
