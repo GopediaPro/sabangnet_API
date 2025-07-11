4ab76889a5a6feefee87cb02725ce4e9dd8449cb
lyckabc <dh2heart@gmail.com>
2025-07-11 14:28:10 +0900

    feat: include ERP macro router in the main application

--- main.py
+++ main.py
@@ -21,6 +21,7 @@ from utils.sabangnet_logger import get_logger, HTTPLoggingMiddleware
 from api.v1.endpoints.one_one_price import router as one_one_price_router
 from api.product_registration_api import router as product_registration_router
 from api.v1.endpoints.down_form_order import router as down_form_order_router
+from api.v1.endpoints.erp_macro import router as erp_macro_router
 
 logger = get_logger(__name__)
 
@@ -56,6 +57,7 @@ master_router.include_router(mall_price_router)
 master_router.include_router(one_one_price_router)
 master_router.include_router(order_router)
 master_router.include_router(down_form_order_router)
+master_router.include_router(erp_macro_router)
 
 app.include_router(master_router)
 app.include_router(product_registration_router)
