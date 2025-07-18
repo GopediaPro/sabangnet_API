7905e75da600d7e7f60beece6f69c03a62221fd4
김병민 <bmkim766@gmail.com>
2025-07-18 21:33:42 +0900

    fix: remove unnecessary convert_int_column calls in ERP macro m_cloumn delete

--- utils/macros/ERP/brandi_erp_macro.py
+++ utils/macros/ERP/brandi_erp_macro.py
@@ -32,7 +32,6 @@ class ERPBrandiMacro:
             col_h.e_column(ws[f"E{row}"])
             col_h.convert_int_column(ws[f"P{row}"])
             col_h.convert_int_column(ws[f"P{row}"])
-            col_h.convert_int_column(ws[f"W{row}"])
         print(f'[{ws.title}] 서식 적용 완료')
 
         output_path = self.ex.save_file(self.file_path)
