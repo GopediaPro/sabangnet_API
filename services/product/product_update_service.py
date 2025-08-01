122bde47dab21bd3e67f02914ebf4de7f0807b24
lyckabc <dh2heart@gmail.com>
2025-08-01 10:36:41 +0900

    refactor: Remove unused modify_product_name method from ProductUpdateService
    - Eliminated the `modify_product_name` method to streamline the ProductUpdateService and enhance code maintainability.
    - This change reduces complexity and focuses on essential product update functionalities.

--- services/product/product_update_service.py
+++ services/product/product_update_service.py
@@ -40,19 +40,3 @@ class ProductUpdateService:
         return await self.product_repository.update_product_ids_by_compayny_goods_cd_with_bracket_removal_batch(
             compayny_goods_cd_and_product_ids
         )
-
-    async def modify_product_name(self, compayny_goods_cd: str, product_name: str) -> ModifiedProductDataDto:
-        product_raw_data = await self.product_repository.find_product_raw_data_by_company_goods_cd(compayny_goods_cd)
-        if product_raw_data is None:
-            raise ValueError(f"Product raw data not found: {compayny_goods_cd}")
-        
-        modified_product_data = await self.product_repository.\
-            find_modified_product_data_by_product_raw_data_id(product_raw_data.id)
-        
-        rev = 0 if modified_product_data is None else modified_product_data.rev + 1
-        
-        result = await self.product_repository.save_modified_product_name(
-            product_raw_data=product_raw_data, rev=rev, product_name=product_name)
-        
-        dto = ModifiedProductDataDto.model_validate(result)
-        return dto
