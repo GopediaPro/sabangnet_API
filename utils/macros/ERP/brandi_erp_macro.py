f5ae16903f989f105ca01dd8066c11021b7f3f09
lyckabc <dh2heart@gmail.com>
2025-08-11 19:55:57 +0900

    refactor: replace 장바구니번호 to 주문번호 세트주문건 조회  & 제주 관련코드 삭제 to 공통 제주관련 코드 관리average_duplicate_cart_address_amounts to average_duplicate_order_address_amounts in brandi_erp_macro.py
    - Replaced the function call for averaging amounts based on cart addresses to reflect order addresses.
    - Removed the unused _jeju_address_column method to streamline the code.

--- utils/macros/ERP/brandi_erp_macro.py
+++ utils/macros/ERP/brandi_erp_macro.py
@@ -1,6 +1,6 @@
 from utils.excels.excel_handler import ExcelHandler
 from utils.excels.excel_column_handler import ExcelColumnHandler
-from utils.macros.ERP.utils import average_duplicate_cart_address_amounts
+from utils.macros.ERP.utils import average_duplicate_order_address_amounts
 
 
 class ERPBrandiMacro:
@@ -30,25 +30,14 @@ class ERPBrandiMacro:
             col_h.h_i_column(ws[f"H{row}"])
             col_h.h_i_column(ws[f"I{row}"])
             col_h.a_formula_column(ws[f"A{row}"])
-            self._jeju_address_column(ws, row, ws[f"J{row}"])  # 확인필요
             col_h.e_column(ws[f"E{row}"])
             col_h.convert_int_column(ws[f"P{row}"])
         print(f'[{ws.title}] 서식 적용 완료')
 
         # 장바구니번호와 수취인주소 조합으로 그룹화 후 평균 금액 적용 (스타배송 모드에서만)
         if self.is_star:
-            average_duplicate_cart_address_amounts(self.ws)
+            average_duplicate_order_address_amounts(self.ws)
 
         output_path = self.ex.save_file(self.file_path)
         print(f"브랜디 ERP 자동화 완료!\n처리된 파일: {output_path}")
         return output_path
-
-    def _jeju_address_column(self, ws, row, cell):
-        """
-        제주 주소 포맷
-        args:
-            cell: 대상 셀 (J)
-        """
-        if cell.value and "제주" in str(cell.value):
-            self.ex.process_jeju_address(
-                ws, row, f_col='F', j_col='J')
