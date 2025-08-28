f15e71c789ff2acd5ad61a00826dc3108d3260b1
김병민 <bmkim766@gmail.com>
2025-08-28 22:26:32 +0900

    fix: Rename mall_id to fld_dsp in DownFormOrdersFromReceiveOrdersDto

--- schemas/down_form_orders/down_form_order_dto.py
+++ schemas/down_form_orders/down_form_order_dto.py
@@ -120,7 +120,7 @@ class DownFormOrdersInvoiceNoUpdateDto(BaseDTO):
 
 class DownFormOrdersFromReceiveOrdersDto(BaseDTO):
     success: Optional[bool] = Field(None, description="성공 여부")
-    mall_id: Optional[str] = Field(None, description="쇼핑몰 아이디")
+    fld_dsp: Optional[str] = Field(None, description="사이트 명")
     dpartner_id: Optional[str] = Field(None, description="배송구분(일반배송, 스타배송)")
     processed_count: Optional[int] = Field(None, description="처리된 데이터 수")
     saved_count: Optional[int] = Field(None, description="저장된 데이터 수")
