23dfe7c26f37a9e5aa6ba2ed3d0e8f38f34bda9d
lyckabc <dh2heart@gmail.com>
2025-09-09 08:04:27 +0900

    feat: Add Ecount ERP v2 router to main application
    - Included the Ecount ERP v2 router in the main application to facilitate routing for new API endpoints.
    - Updated the master router to include the new Ecount ERP v2 functionality, enhancing the overall API structure.

--- main.py
+++ main.py
@@ -33,6 +33,7 @@ from api.v2.endpoints.down_form_order import router as down_form_order_v2_router
 from api.v2.endpoints.smile.smile_macro import router as smile_macro_v2_router
 from api.v2.endpoints.ecount.erp_excel_import import router as ecount_excel_import_v2_router
 from api.v2.endpoints.ecount.erp_transfer import router as ecount_erp_transfer_v2_router
+from api.v2.endpoints.ecount.erp import router as ecount_erp_v2_router
 
 
 from utils.logs.sabangnet_logger import get_logger, HTTPLoggingMiddleware
@@ -93,6 +94,7 @@ master_router_v2.include_router(down_form_order_v2_router)
 master_router_v2.include_router(smile_macro_v2_router)
 master_router_v2.include_router(ecount_excel_import_v2_router)
 master_router_v2.include_router(ecount_erp_transfer_v2_router)
+master_router_v2.include_router(ecount_erp_v2_router)
 
 
 app.include_router(master_router)
